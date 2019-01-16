34d54be90fa8676432eeecac65cd00f154b371c9
xMokAx <ahmedmokhtar11j@gmail.com>
2019-01-16 22:36:17 +0200

    Updates

---
+++ precache-manifest.4e8e2c106643aaeafb2c2640cc4c3aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d271e45b59d6671f4861e194be0a0eec",
+    "url": "/map-app/static/media/logo.d271e45b.png"
+  },
+  {
+    "revision": "101e744af21f3692db7d",
+    "url": "/map-app/static/js/runtime~main.101e744a.js"
+  },
+  {
+    "revision": "6d7f97a5f1731112ddb0",
+    "url": "/map-app/static/js/main.6d7f97a5.chunk.js"
+  },
+  {
+    "revision": "f7d3e33438e3662400a3",
+    "url": "/map-app/static/js/1.f7d3e334.chunk.js"
+  },
+  {
+    "revision": "6d7f97a5f1731112ddb0",
+    "url": "/map-app/static/css/main.b9666c49.chunk.css"
+  },
+  {
+    "revision": "4a93783fe5e27b98010ca46ff766c10c",
+    "url": "/map-app/index.html"
+  }
+];
